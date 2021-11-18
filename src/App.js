73efa88c734c67c33340b62feba11e0b8a1103ca
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-11-18 14:39:22 +0200

    redux: add store and create auth slice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import AppRouter from 'components/AppLayout';
 
 import 'antd/dist/antd.css';
 import './App.css';
+import { store } from 'redux/store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/constant/fetchStatus.js
@@ -0,0 +1,6 @@
+export const FETCH_STATUS = {
+  IDLE: 'IDLE',
+  LOADING: 'LOADING',
+  SUCCEEDED: 'SUCCEEDED',
+  FAILED: 'FAILED',
+};
