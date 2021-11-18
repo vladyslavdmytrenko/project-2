de2000e7060b9eede73ac247b05ecd1922566770
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-11-18 17:29:53 +0200

    json-server: fix cors

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Expires', 0);
+  next();
+});
 server.use(router);
 server.listen(3001, () => {
   console.log('JSON Server is running');
