9ef1f77213bcff431992806bc53a1c540a12df26
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-11-19 11:36:19 +0200

    fix date format in utils

--- src/utils/DateTime.js
+++ src/utils/DateTime.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
 export const timestampToDate = (value) =>
-  moment.unix(value / 1000).format('MM/DD/YYYY LTS');
+  moment.unix(value / 1000).format('DD/MM/YYYY LTS');
