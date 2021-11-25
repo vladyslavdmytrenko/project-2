d660f7bbd08e49d3f1cdac6eb7cf7519e8a94008
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-11-25 18:05:22 +0200

    add validation for appointment form

--- src/components/Appointment/Form/index.js
+++ src/components/Appointment/Form/index.js
@@ -11,59 +11,97 @@ import {
 } from 'antd';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+import moment from 'moment';
+import NumberFormat from 'react-number-format';
 
 import style from './AppointmentForm.module.css';
 
 const formatTime = 'HH:mm';
 const formatDate = 'MM/DD/YYYY';
-const wrapperLayoutInput = {
-  span: 24,
-};
 
 const validationSchema = yup.object({
   firstName: yup
     .string()
     .max(100, 'Maximum is 100 characters')
     .required('Required'),
-  lastName: yup
+
+  secondName: yup
     .string()
     .max(100, 'Maximum is 100 characters')
     .required('Required'),
-  appointmentDate: yup.date().required('Required'),
-  appointmentTime: yup.date().required('Required'),
+
+  appointmentDate: yup
+    .string()
+    .nullable()
+    .required('Required')
+    .test(
+      'dateTest',
+      'Invalid date',
+      (value) =>
+        moment(value).format(formatDate) > moment(Date.now()).format(formatDate)
+    ),
+
+  appointmentTime: yup
+    .string()
+    .nullable()
+    .required('Required')
+    .test(
+      'timeTest',
+      'Invalid date',
+      (value) =>
+        moment(value).format(formatTime) > moment(Date.now()).format(formatTime)
+    ),
+
+  phoneNumber: yup
+    .string()
+    .matches(/\(\d{3}\)-\d{3}-\d{4}/g, 'Phone number is not valid')
+    .required('Required'),
+
+  notes: yup.string().max(500),
 });
 
 const AppointmentForm = (props) => {
+  const initialValues = {
+    firstName: '',
+    secondName: '',
+    appointmentDate: '',
+    appointmentTime: '',
+    phoneNumber: '',
+    notes: '',
+    department: null,
+  };
+
   const {
     handleChange,
     handleBlur,
     handleSubmit,
     resetForm,
+    setFieldValue,
+    setFieldTouched,
     values,
     touched,
     errors,
+    isValid,
   } = useFormik({
-    initialValues: {
-      firstName: 'first name',
-      secondName: '',
-      appointmentDate: '',
-      appointmentTime: '',
-    },
+    isInitialValid: false,
+    initialValues,
     validationSchema,
     onSubmit: (values) => console.log(values),
   });
-  console.log(values, touched, errors);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <Form layout="vertical" autoComplete="off" onFinish={handleSubmit}>
       <Row justify="space-around">
         <Col span={10}>
           <Form.Item
-            name="First Name"
+            name="firstName"
             label="First Name"
-            wrapperCol={wrapperLayoutInput}
-            help={touched.firstName && errors.firstName ? errors.firstName : ''}
+            hasFeedback={touched.firstName}
+            help={
+              touched.firstName && errors.firstName ? errors.firstName : false
+            }
             validateStatus={
-              touched.firstName && errors.firstName ? 'error' : undefined
+              touched.firstName && errors.firstName ? 'error' : 'success'
             }
             required
           >
@@ -78,37 +116,60 @@ const AppointmentForm = (props) => {
 
           <Row justify="space-between" align="bottom" wrap={false}>
             <Form.Item
-              name="Appointment Date and Time"
               label="Appointment Date and Time"
+              hasFeedback={touched.appointmentDate}
               help={
                 touched.appointmentDate && errors.appointmentDate
                   ? errors.appointmentDate
-                  : ''
+                  : false
               }
               validateStatus={
                 touched.appointmentDate && errors.appointmentDate
                   ? 'error'
-                  : undefined
+                  : 'success'
               }
+              required
             >
               <DatePicker
-                input={style.input}
+                className={style.input}
+                allowClear={false}
                 format={formatDate}
-                onChange={handleChange}
-                onBlur={handleBlur}
+                onChange={(date) => {
+                  setFieldValue('appointmentDate', date);
+                }}
+                onBlur={() => setFieldTouched('appointmentDate', true)}
                 value={values.appointmentDate}
               />
             </Form.Item>
-            <Form.Item input={style.input} name="Appointment time *">
-              <TimePicker input={style.input} format={formatTime} />
+
+            <Form.Item
+              hasFeedback={touched.appointmentTime}
+              help={
+                touched.appointmentTime && errors.appointmentTime
+                  ? errors.appointmentTime
+                  : false
+              }
+              validateStatus={
+                touched.appointmentTime && errors.appointmentTime
+                  ? 'error'
+                  : 'success'
+              }
+              required
+            >
+              <TimePicker
+                className={style.input}
+                allowClear={false}
+                format={formatTime}
+                onChange={(time) => {
+                  setFieldValue('appointmentTime', time);
+                }}
+                onBlur={() => setFieldTouched('appointmentTime')}
+                value={values.appointmentTime}
+              />
             </Form.Item>
           </Row>
 
-          <Form.Item
-            name="Department *"
-            label="Department *"
-            wrapperCol={wrapperLayoutInput}
-          >
+          <Form.Item label="Department" required>
             <Select placeholder="Select Department">
               <Select.Option value="jack">Jack</Select.Option>
               <Select.Option value="lucy">Lucy</Select.Option>
@@ -119,36 +180,85 @@ const AppointmentForm = (props) => {
 
         <Col span={10}>
           <Form.Item
-            name="Second Name *"
-            label="Second Name *"
-            wrapperCol={wrapperLayoutInput}
+            label="Second Name"
+            hasFeedback={touched.secondName}
+            help={
+              touched.secondName && errors.secondName
+                ? errors.secondName
+                : false
+            }
+            validateStatus={
+              touched.secondName && errors.secondName ? 'error' : 'success'
+            }
+            required
           >
-            <Input placeholder="Enter second name" />
+            <Input
+              placeholder="Enter second name"
+              name="secondName"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              valuer={values.secondName}
+            />
           </Form.Item>
 
           <Form.Item
-            name="Phone number *"
-            label="Phone number *"
-            wrapperCol={wrapperLayoutInput}
+            label="Phone number"
+            hasFeedback={touched.phoneNumber}
+            help={
+              touched.phoneNumber && errors.phoneNumber
+                ? errors.phoneNumber
+                : false
+            }
+            validateStatus={
+              touched.phoneNumber && errors.phoneNumber ? 'error' : 'success'
+            }
+            required
           >
-            <Input type="number" placeholder="(___)-____-___)" />
+            <NumberFormat
+              name="phoneNumber"
+              type="text"
+              format="(###)-###-####"
+              placeholder="(###)-###-####"
+              mask="_"
+              customInput={Input}
+              onChange={(e) => {
+                setFieldValue('phoneNumber', e.target.value);
+              }}
+              onBlur={handleBlur}
+              value={values.phoneNumber}
+            />
           </Form.Item>
 
-          <Form.Item name="Notes" label="Notes" wrapperCol={wrapperLayoutInput}>
-            <Input.TextArea placeholder="Enter notes" />
+          <Form.Item
+            name="notes"
+            label="Notes"
+            hasFeedback={touched.notes}
+            help={touched.notes && errors.notes ? errors.notes : false}
+            validateStatus={touched.notes && errors.notes ? 'error' : 'success'}
+          >
+            <Input.TextArea
+              name="notes"
+              placeholder="Enter notes"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.notes}
+            />
           </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 14, span: 10 }}>
-            <Button type="primary" htmlType="submit">
+          <Form.Item>
+            <Button type="primary" htmlType="submit" disabled={!isValid}>
               Submit
             </Button>
-            <Button htmlType="button" onClick={resetForm}>
+            <Button
+              htmlType="button"
+              onClick={() => resetForm({ values: initialValues })}
+            >
               Cancel
             </Button>
           </Form.Item>
         </Col>
       </Row>
-    </form>
+    </Form>
   );
 };
 
--- src/constant/index.js
+++ src/constant/index.js
@@ -1,1 +1,2 @@
 export { FETCH_STATUS } from './fetchStatus';
+export const DATE_FORMATE = 'DD/MM/YYYY LTS';
