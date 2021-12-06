92545c93f04ed37f3b08fca5950e9e48da274c0f
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-12-06 15:34:37 +0200

    utils: remove unix transformation

--- src/utils/DateTime.js
+++ src/utils/DateTime.js
@@ -2,15 +2,13 @@ import moment from 'moment';
 import { DATE_FORMAT, TIME_FORMAT } from 'constant';
 
 export const timestampToDateTime = (value) =>
-  moment.unix(value / 1000).format('DD/MM/YYYY LTS');
+  moment(+value).format('DD/MM/YYYY LTS');
 
-export const timestampToMoment = (value) => moment.unix(value / 1000);
+export const timestampToMoment = (value) => moment(+value);
 
-export const timestampToDate = (value) =>
-  moment.unix(value / 1000).format('DD/MM/YYYY');
+export const timestampToDate = (value) => moment(+value).format('DD/MM/YYYY');
 
-export const timestampToTime = (value) =>
-  moment.unix(value / 1000).format('HH:MM');
+export const timestampToTime = (value) => moment(+value).format('HH:MM');
 
 export const dateTimeToTimestamp = (data, time) =>
   moment(`${data.format(DATE_FORMAT)} ${time.format(TIME_FORMAT)}`).format('x');
